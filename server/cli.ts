3b9024d33f176b2452fafdd1776d60ac6dcf5690
MotorTruck1221 <motortruck1221@protonmail.com>
2025-02-22 00:17:04 -0700

    CLI: start to add an 'upgrade' command

--- server/cli.ts
+++ server/cli.ts
@@ -8,40 +8,101 @@ import { startServer as HonoServer } from './standalone/standalone.ts';
 import { startServer as FastifyServer } from './full/server.ts';
 import { fromFileUrl } from 'jsr:@std/path';
 
+import packageJSON from "../package.json" with { type: 'json' };
+const { version } = packageJSON;
+
 interface CLIArgs {
+    _: any[];
     help: boolean;
-    server?: "full" | "standalone";
+};
+
+interface StartArgs extends CLIArgs {
+    server: "full" | "standalone";
     config?: string;
     seo?: boolean;
-}
+};
 
-const args = parseArgs(Deno.args, {
-    boolean: ["help", "seo"],
-    string: ["server", "config"]
-}) as CLIArgs;
+interface UpgradeArgs extends CLIArgs {};
+
+interface DescriptionType {
+    name: string;
+    description: string;
+    default?: string;
+};
+
+class CLI<T extends CLIArgs> {
+    #args: T;
+    constructor(config: { booleans: string[], strings: string[] }) {
+        this.#args = parseArgs(Deno.args, {
+            boolean: config.booleans,
+            string: config.strings
+        }) as T;    
+    }
+
+    #white(text: string) {
+        return chalk.whiteBright.bold(text);
+    }
+
+    getArgs(): T {
+        return this.#args;
+    }
+
+    help(descriptions?: DescriptionType[]) {
+        console.log(chalk.whiteBright.bold('--help - Trigger this menu'));
+        descriptions?.map((val) => console.log(`${this.#white('--')}${this.#white(val.name)} ${this.#white('-')} ${this.#white(val.description)} ${val.default ? this.#white(`(default: ${val.default})`) : ''}`))
+        Deno.exit();
+    }
+
+    async start(fn: () => unknown) {
+        await fn();
+    }
+}
 
 console.log(gradient(Object.values(messageColors)).multiline(message));
-console.log(chalk.hex('#34b874')('Welcome to the Incognito CLI!'));
-if (args.help || (!args.server && !args.config)) {
-    console.log(`
-        ${chalk.whiteBright.bold('--help - Trigger this menu')}
-        ${chalk.whiteBright.bold('--server <full|standalone> - Select the server type (default: full)')}
-        ${chalk.whiteBright.bold('--config <path> - The path to a custom config. (default: config.toml)')}
-        ${chalk.whiteBright.bold('--seo - Enable SEO website regardless of the config file (default: false)')}
-    `)
+console.log(chalk.hex('#34b874')(`Welcome to the Incognito CLI!\nVersion: ${version}\n`));
+
+const cli = new CLI<CLIArgs>({
+    booleans: [], strings: []
+});
+
+const args = cli.getArgs();
+
+if (args._.length !== 0 && args._[0] !== "upgrade") {
+    console.log(chalk.redBright.bold(`The command ${args._[0]} is not available.\nPlease use a valid command`))
     Deno.exit();
 }
 
-if (args.config) {
-    const path = await Deno.realPath(args.config);
-    args.config = path;
-}
-
-if (args.server !== "standalone" && args.server !== "full") {
-    console.log(chalk.redBright.bold('Error with options --server\n The only options available are: full or standalone'));
+if (args._[0] === "upgrade") {
+    const upgradeCLI = new CLI<UpgradeArgs>({ booleans: [], strings: [] });
+    await upgradeCLI.start(() => {});
     Deno.exit();
 }
 
-args.server === "standalone" 
-    ? await HonoServer(args.config ?? fromFileUrl(new URL('../config.toml', import.meta.url)), args.seo) 
-    : await FastifyServer(args.config ?? fromFileUrl(new URL('../config.toml', import.meta.url)), args.seo);
+const startCLI = new CLI<StartArgs>({ 
+    booleans: ["help", "seo"], strings: ["config"] 
+});
+
+const startArgs = startCLI.getArgs();
+
+if (startArgs.help || !startArgs.server) {
+    startCLI.help([
+        { name: "server [full|standalone]", description: "Select the server type", default: "full" },
+        { name: "config [path]", description: "The path to a custom config", default: 'config.toml' },
+        { name: "seo", description: "Enable SEO website regardless of the config file", default: 'false' }
+    ]);
+}
+
+if (startArgs.config) {
+    const path = await Deno.realPath(startArgs.config);
+    startArgs.config = path;
+}
+
+if (startArgs.server !== "full" && startArgs.server !== "standalone") {
+    console.log(chalk.redBright.bold(`Error with option --server!\nThe value: "${startArgs.server}" is not valid!\nThe only available options are: "full" or "standalone"`));
+}
+
+startCLI.start(async () => {
+    startArgs.server === "standalone"
+        ? await HonoServer(startArgs.config ?? fromFileUrl(new URL('../config.toml', import.meta.url)), startArgs.seo)
+        : await FastifyServer(startArgs.config ?? fromFileUrl(new URL('../config.toml', import.meta.url)), startArgs.seo)
+});
